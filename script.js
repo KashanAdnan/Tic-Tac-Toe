9aaf17fc28b5be6cd90c0bcc7e1be5db3fa9927f
KashanAdnan <khanzaidaboy@gmai.com>
2022-11-01 22:08:03 -0700

    some-code

---
+++ script.js
@@ -0,0 +1,60 @@
+// ---------- Defining Components ---------- //
+let playerText = document.getElementById("playerText");
+let restartBtn = document.getElementById("restartBtn");
+let boxes = Array.from(document.getElementsByClassName("xo"));
+const O = "0";
+const X = "X";
+let currPlayer = X;
+let spaces = Array(9).fill(null);
+
+// --------- Making A Start Game Function When on Click on the box text that i define in components ---------- //
+const starGame = () => {
+  boxes.forEach((box) => box.addEventListener("click", boxClicked));
+};
+const boxClicked = (e) => {
+  console.log(e)
+  const id = e.target.id;
+  if (!spaces[id]) {
+    spaces[id] = currPlayer;
+    e.target.innerText = currPlayer;
+    if (won() !== false) {
+      playerText.innerText = `${currPlayer} Has Won !`;
+    }
+    currPlayer = currPlayer == O ? X : O;
+  }
+};
+// --------- Making A Win Game Function When User Won  ---------- //
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 5],
+  [6, 4, 2],
+  [0,4,9],
+];
+
+function won() {
+  for (const condition of win) {
+    let [a, b, c] = condition;
+    if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+      return [a, b, c];
+    }
+  }
+  return false;
+}
+
+restartBtn.addEventListener("click", restart);
+function restart() {
+  spaces.fill(null);
+  boxes.forEach((box) => {
+    box.innerText = " ";
+  });
+  playerText.innerText = `Tic Tac Toe Game`;
+  currPlayer = X;
+}
+
+starGame();
+
